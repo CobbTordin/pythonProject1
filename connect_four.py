b9e75a0e2b004c9036e228925be95ab54dba09ef
Todd <107519104+CobbTordin@users.noreply.github.com>
2022-08-30 20:39:17 -0400

    Update connect_four.py

--- connect_four.py
+++ connect_four.py
@@ -1,40 +1,100 @@
-board = [["O", "O", "O", "O", "O", "O", "O"],
-         ["O", "O", "O", "O", "O", "O", "O"],
-         ["O", "O", "O", "O", "O", "O", "O"],
-         ["O", "O", "O", "O", "O", "O", "O"],
-         ["O", "O", "O", "O", "O", "O", "O"],
-         ["O", "O", "O", "O", "O", "O", "O"]]
+import numpy as np
 
-bot_nums = [5, 5, 5, 5, 5, 5, 5]
 
-while True:
-    player1 = input("Enter initial of player 1:\n> ")
-    if len(player1) > 1 or not player1.isalpha():
-        print("Initial should be one letter.")
-        continue
-    break
-while True:
-    player2 = input("Enter initial of player 2:\n> ")
-    if len(player2) > 1 or not player2.isalpha():
-        print("Initial should be one letter.")
-        continue
-    break
+def present():
+    global board
+    print(str(board).replace(' [', '').replace('[', '').replace(']', '').replace("'", ""))
 
-players = [player1, player2]
 
 while True:
-    for player in players:
-        for row in board:
-            print("".join(row))
-        print("Enter a column between 1 and 7")
-        while True:
-            inp_col = (int(input()) - 1)
-            if inp_col > 7:
-                print("Enter a column between 1 and 7")
+    board = np.array(
+        [["-", "-", "-", "-", "-", "-",],
+         ["-", "-", "-", "-", "-", "-",],
+         ["-", "-", "-", "-", "-", "-",],
+         ["-", "-", "-", "-", "-", "-",],
+         ["-", "-", "-", "-", "-", "-",],
+         ["-", "-", "-", "-", "-", "-",]]
+    )
+
+    bottom_row = [0, 0, 0, 0, 0, 0]
+
+    players = ("X", "O")
+    winner = None
+    game_over = False
+
+    present()
+    while True:
+        for player in players:
+            four_in_a_row = []
+            for i in range(4):
+                four_in_a_row.append(player)
+            four_in_a_row = "".join(four_in_a_row)
+            if bottom_row == [6, 6, 6, 6, 6, 6]:
+                game_over = True
+                break
+            while True:
+                move = int(input(f"\n{player}'s turn: "))
+                if bottom_row[move - 1] >= 6:
+                    print("That column is full. Please try again.")
+                else:
+                    break
+            board[5 - bottom_row[move - 1]][move - 1] = player        
+            present()
+            for row in board:
+                if four_in_a_row in "".join(row):
+                    winner = player
+                    game_over = True
+                    break
+            columns = []
+            for i in range(len(board)):
+                columns.append(board[:, i])
+            for col in columns:
+                if four_in_a_row in "".join(col):
+                    winner = player
+                    game_over = True
+                    break
+            asc_diags = []
+            for idx, space in np.ndenumerate(board):
+                asc_diag_chunk = []
+                for i in range(4):
+                    try:
+                        asc_diag_chunk.append(board[idx[0] - i][idx[1] + i])
+                    except IndexError:
+                        asc_diag_chunk.append("-")
+                asc_diags.append(asc_diag_chunk)
+            for diag in asc_diags:
+                if four_in_a_row in "".join(diag):
+                    winner = player
+                    game_over = True
+                    break
+            desc_diags = []
+            for idx, space in np.ndenumerate(board):
+                desc_diag_chunk = []
+                for i in range(4):
+                    try:
+                        desc_diag_chunk.append(board[idx[0] + i][idx[1] + i])
+                    except IndexError:
+                        desc_diag_chunk.append("-")
+                desc_diags.append(desc_diag_chunk)
+            for diag in desc_diags:
+                if four_in_a_row in "".join(diag):
+                    winner = player
+                    game_over = True
+                    break
+            bottom_row[move - 1] += 1
+            if winner is None:
                 continue
-            if bot_nums[inp_col] == -1:
-                print("No room left in that column")
-                continue
+            else:
+                break
+        if game_over is True:
             break
-        board[bot_nums[inp_col]][inp_col] = player
-        bot_nums[inp_col] -= 1
+    if winner is None:
+        print("\nDraw!")
+    else:
+        print(f"\n{winner} wins!")
+    new_game = input("Play again? y/n ")
+    if new_game == "y":
+        continue
+    else:
+        break
+
